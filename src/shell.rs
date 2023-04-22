f0a909e8d90555cc97e6934d63331dc8335c12c5
phant80m <black.pandazayd@gmail.com>
2023-04-22 13:38:55 +1000

    added user env variable to checkmate_demo var

--- src/shell.rs
+++ src/shell.rs
@@ -25,8 +25,8 @@ pub fn shell() {
     let cwd = std::env::current_dir().unwrap();
     let cwd_string = cwd.to_string_lossy().to_string().replace(['\\', '/'], "");
     let host = hostname::get().unwrap_or_else(|_| "unknown".to_string().into());
-    let host_string = host.to_string_lossy().to_string();
-    let checkmate_demo = "Checkmate @";
+    let _host_string = host.to_string_lossy().to_string();
+    let checkmate_demo = format!("{} @", env!("USER"));
 
     print!(
         "{}{}{}{}{}",
@@ -34,7 +34,7 @@ pub fn shell() {
         checkmate_demo.black().bold().on_bright_white(),
         "".black().bright_white().bold().on_green(),
         cwd_string.black().to_string().on_green(),
-        "".green().bold()
+        " ".green().bold()
     );
     io::stdout().flush().unwrap();
 
@@ -46,81 +46,7 @@ pub fn shell() {
 
     match command {
         // dir command
-        "dir" | "ls" => {
-            let output = directory();
-            println!("{}", output);
-        }
-
-        // exit feature
-        "exit" | "quit" | "q" => std::process::exit(0),
-
-        // clear screem
-        "clear" | "cls" => {
-            print!("\x1B[2J\x1B[1;1H");
-            io::stdout().flush().unwrap();
-        }
-
-        // echo command (useless rn)
-        command if command.starts_with("echo ") => {
-            let echo_out = &command[5..];
-            println!("{}", echo_out);
-        }
-
-        // change dir! works with shell prompt
-        command if command.starts_with("cd ") => {
-            let path = &command[3..];
-            if let Err(e) = std::env::set_current_dir(path) {
-                eprintln!("Failed to find directory {}", e)
-            }
-        }
-        // mkdir feature
-        command if command.starts_with("mkdir ") => {
-            let path = &command[6..];
-            match std::fs::create_dir(path) {
-                Ok(_) => println!("Created directory {}", path),
-                Err(_) => eprintln!("failed to find directory"),
-            }
-        }
-        // remove directory
-        command if command.starts_with("rmdir ") => {
-            let path = &command[6..];
-            match std::fs::remove_dir(path) {
-                Ok(_) => println!("removed directory {}", path),
-                Err(_) => eprintln!("cannot find directory specified"),
-            }
-        }
-
-        // remove file (NOT REMOVE DIRECTORY)
-        command if command.starts_with("rm ") => {
-            let path = &command[3..];
-            match std::fs::remove_file(path) {
-                Ok(_) => println!("deleted {}", path),
-                Err(_) => eprintln!("failed to delete file."),
-            }
-        }
-
-        // create file, Different to makedir
-        command if command.starts_with("mk ") => {
-            let path = &command[3..];
-            match std::fs::File::create(path) {
-                Ok(_) => println!("Created {}", path),
-                Err(_) => eprintln!("failed to create file."),
-            }
-        }
-
-        // cat command (for reading files)
-        command if command.starts_with("cat ") => {
-            let path = &command[4..];
-            match std::fs::read_to_string(path) {
-                Ok(contents) => {
-                    println!("{}", contents);
-                }
-                Err(err) => {
-                    eprintln!("error {}", err);
-                }
-            }
-        }
-        // no cmd
+                // no cmd
         "" => {}
         _ => {
             use std::io::{BufRead, BufReader};
